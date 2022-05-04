b46a5e745d12d04704288074a8cfd8b50ec8270c
Martin Gaňo <martin.gano@espressif.com>
2022-05-04 23:26:14 +0200

    add paths generator

---
+++ data/generate_augmented_paths.py
@@ -0,0 +1,15 @@
+import os
+
+from data_generator import yield_png
+
+
+def generate_augmented_paths(augmented_data_folder,
+                             augmented_paths_filename="augmented_images_paths.txt"):
+    augmented_paths_file_ = open(augmented_paths_filename, "w")
+    for png_file_ in yield_png(augmented_data_folder):
+        augmented_paths_file_.write(os.path.join("data", augmented_data_folder, png_file_))
+        augmented_paths_file_.write("\n")
+
+
+if __name__ == '__main__':
+    generate_augmented_paths("augmented_data")
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,43 @@
 //
 
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <fstream>
 
 using namespace std;
+using namespace cv;
+
+Mat readFile(const string &image_path) {
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    if (img.empty()) {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        exit(1);
+    }
+    return img;
+}
 
 int main(int argc, char **argv) {
-    cout << "Hello Brno!" << endl;
+
+
+
+    ifstream paths("data/augmented_images_paths.txt", ios_base::in);
+    while (paths) {
+        string png_path;
+        paths >> png_path;
+        if(!png_path.empty()){
+            Mat img = readFile(png_path);
+
+            imshow("Display window", img);
+            int k = waitKey(0); // Wait for a keystroke in the window
+            if (k == 's') {
+                imwrite("starry_night.png", img);
+            }
+        }
+
+    }
+
+
 }
